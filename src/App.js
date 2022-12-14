936f0200bc0922e4e7d1ee88200bf82793332c24
Ewa Hostynska <38616184+19101990@users.noreply.github.com>
2022-12-14 19:53:27 +0100

    add basic calculations

--- src/App.js
+++ src/App.js
@@ -3,12 +3,98 @@ import React, { useState } from "react";
 import Button from './Button.js'
 
 function App() {
+  const [operator, setOperator] = useState(null);
   const [oldValue, setOldValue] = useState(null);
   const [newValue, setNewValue] = useState("0");
 
+  const handleOperations = () => {
+    let result;
+    switch(operator) {
+      case '+':
+        result = parseInt(oldValue) + parseInt(newValue);
+        console.log(result)
+        break;
+      case '-':
+        result = oldValue - newValue;
+        console.log(result)
+        break;
+      case '*':
+        result = oldValue * newValue;
+        console.log(result)
+        break;
+      case '/':
+        result = oldValue / newValue;
+        console.log(result)
+        break;
+    }
+    setNewValue(result);
+    setOldValue(null);
+    setOperator(null);
+  }
   const handleCalculation = (buttonValue) => {
-    console.log(buttonValue)
-    setNewValue(String(buttonValue));
+    if(newValue === "0") {
+      switch(buttonValue) {
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+          setNewValue(String(buttonValue));
+          break;
+        case '0':
+          return newValue !== "0" ? setNewValue(String(buttonValue)) : '';
+        case 'AC':
+          setOldValue(null);
+          setNewValue("0");
+          break;
+      }
+    } else {
+      switch(buttonValue) {
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+        case '0':
+          setNewValue(newValue + String(buttonValue));
+          break;
+        case 'AC':
+          setOldValue(null);
+          setNewValue("0");
+          break;
+        case '/':
+          setOldValue(newValue);
+          setNewValue("0");
+          setOperator("/")
+          break;
+        case '*':
+          setOldValue(newValue);
+          setNewValue("0");
+          setOperator("*")
+          break;
+        case '-':
+          setOldValue(newValue);
+          setNewValue("0");
+          setOperator("-")
+          break;
+        case '+':
+          setOldValue(newValue);
+          setNewValue("0");
+          setOperator("+")
+          break;
+        case '=':
+          handleOperations();
+          break;
+      }
+    }
   }
   return (
     <div className="calculator">
